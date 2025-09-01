db42fe3e457dd220feb702dfd7070deb59e59ecd
Kirk Matt <kirkmatt@proton.me>
2025-09-01 21:52:26 +0100

    removed unneeded files

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Text.Regex.TDFA
+
+emailRegex = "[a-zA-Z0-9+._-]+@[a-zA-Z-]+\\.[a-z]+"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main
+    | "my email is email@email.com" =~ emailRegex = putStrLn "Hello, Haskell!"
+    | otherwise = putStrLn "failed"
