6eced80f970b5cd40f474b2ffd1b1dcfe1f8d02c
Kirk Matt <kirkmatt@proton.me>
2025-09-13 21:25:37 +0100

    Initial cli checking

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
+import System.Environment (getArgs)
 import Text.Regex.TDFA
 
 emailRegex = "[a-zA-Z0-9+._-]+@[a-zA-Z-]+\\.[a-z]+"
 
 main :: IO ()
-main
-    | "my email is email@email.com" =~ emailRegex = putStrLn "Hello, Haskell!"
-    | otherwise = putStrLn "failed"
+main = do
+  args <- getArgs
+  case args of
+    [regex, string] -> checkInput regex string
+    _ -> putStrLn "needs 2 arguments"
+
+checkInput regex string
+  | string =~ regex = putStrLn " works!"
+  | otherwise = putStrLn "ensure the string mathces the regex"
