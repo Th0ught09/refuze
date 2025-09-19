8402b0584742063e6789c80ded769eab32a0216a
Kirk Matt <kirkmatt@proton.me>
2025-09-19 20:29:32 +0100

    added randomisation

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,20 @@ module Main where
 
 import System.Environment (getArgs)
 import Text.Regex.TDFA
+import System.Random
 
 emailRegex = "[a-zA-Z0-9+._-]+@[a-zA-Z-]+\\.[a-z]+"
 
 main :: IO ()
 main = do
+  g <- getStdGen
+  print $ take 10 (randomRs ('a', 'z') g)
   args <- getArgs
   case args of
-    [regex, string] -> checkInput regex string
+    [regex, string] -> putStrLn (checkInput regex string)
     _ -> putStrLn "needs 2 arguments"
 
+checkInput :: String -> String -> String
 checkInput regex string
-  | string =~ regex = putStrLn " works!"
-  | otherwise = putStrLn "ensure the string mathces the regex"
+  | string =~ regex = " works!"
+  | otherwise = "ensure the string mathces the regex"
