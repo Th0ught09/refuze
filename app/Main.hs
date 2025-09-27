64595cfec7f227e9bac409d434c5fa2681c0c9e2
Kirk Matt <kirkmatt@proton.me>
2025-09-27 20:01:57 +0100

    fixed io to handle errors correctly

--- app/Main.hs
+++ app/Main.hs
@@ -13,25 +13,58 @@ data RTree a = Nil | Node (RTree a) (RTree a)
 main :: IO ()
 main = do
   a <- randomRIO (1 :: Int, 20)
-  args <- getArguments
-  putStrLn "filler"
-  hPutStrLn stderr "err"
-
-getArguments = do
   args <- getArgs
-  case args of
-    [regex, string] -> checkInput regex, string
-    _ -> exitWith (ExitFailure 2)
+  case processArgs args of
+    0 -> getTreeOutput args
+    1 -> putStrLn "Incorrect number of args"
+    2 -> putStrLn "No regex Match"
+    _ -> putStrLn "Unknown Error"
 
-checkInput :: String -> String -> Int
-checkInput regex string
-  | string =~ regex = 1
+-- getOutput :: Either (RTree a) String -> String
+-- getOutput tree = "matched"
+-- getOutput = "no match"
+-- getArguments = do
+--   args <- getArgs
+--   case args of
+--     [regex, string] -> checkInput regex string
+--     _ -> exitWith (ExitFailure 2)
+-- checkInput :: String -> String -> Int
+-- checkInput regex string
+--   | string =~ regex = 1
+--   | otherwise = exitWith (ExitFailure 1)
+processTree :: Int -> String
+processTree 1 = "does not work"
+processTree 2 = "no match"
+processTree 0 = "works!"
+processTree a = "other"
+
+getTreeOutput :: [String] -> IO ()
+getTreeOutput a =
+  let regex = a !! 1
+      string = a !! 2
+      tree = startTree regex string
+   in do
+        putStrLn $ getLTree tree
+        hPutStrLn stderr $ getRTree tree
+
+getLTree :: RTree a -> String
+getLTree a = "matches"
+
+getRTree :: RTree a -> String
+getRTree a = "non matches"
+
+processArgs :: [String] -> Int
+processArgs a = case a of
+  [regex, string] -> testRegex regex string
+  _ -> 1
+
+testRegex :: String -> String -> Int
+testRegex regex string
+  | string =~ regex = 0
   | otherwise = 2
 
-startTree :: String -> String -> String
-startTree regex string = do
-  -- a <- Node (getNeg regex string) (getPos regex string)
-  "hi"
+startTree :: String -> String -> RTree a
+startTree regex string = Node (getNeg regex string) (getPos regex string)
 
 getNeg :: String -> String -> RTree a
 getNeg regex string = Node Nil Nil
