b0d644fb6044efa20ee58d830818677114c09331
Kirk Matt <kirkmatt@proton.me>
2025-09-29 21:25:37 +0100

    random index and initial code for trees

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,16 @@ import System.IO
 import System.Random
 import Text.Regex.TDFA
 
+pureGen :: StdGen
+pureGen = mkStdGen 137
+
 data Node = String
 
 data RTree a = Nil | Node (RTree a) (RTree a)
 
 main :: IO ()
 main = do
-  a <- randomRIO (1 :: Int, 20)
+  -- a <- randomRIO (1 :: Int, 20)
   args <- getArgs
   processArgs args
 
@@ -28,7 +31,7 @@ testRegex regex string
 
 getTreeOutput :: String -> String -> IO ()
 getTreeOutput regex string =
-  let tree = startTree regex string
+  let tree = startTree regex string 1000
    in do
         putStrLn $ getLTree tree
         hPutStrLn stderr $ getRTree tree
@@ -39,11 +42,31 @@ getLTree a = "matches"
 getRTree :: RTree a -> String
 getRTree a = "non matches"
 
-startTree :: String -> String -> RTree a
-startTree regex string = Node (getNeg regex string) (getPos regex string)
+startTree :: String -> String -> Int -> RTree a
+startTree regex string depth = Node (getNeg regex string depth) (getPos regex string depth)
 
-getNeg :: String -> String -> RTree a
-getNeg regex string = Node Nil Nil
+getNeg :: String -> String -> Int -> RTree a
+getNeg regex string depth = Node Nil Nil
+getNeg regex string 0 = Nil
 
-getPos :: String -> String -> RTree a
-getPos regex string = Node Nil Nil
+badMatch :: String -> String -> String
+badMatch regex string =
+  let index = fst $ uniformR (1 :: Int, length string :: Int) pureGen
+      choice = fst $ uniformR (1 :: Int, 3) pureGen
+   in case choice of
+        1 -> removeChar regex string
+        2 -> addChar regex string
+        _ -> changeChar regex string
+
+removeChar :: String -> String -> String
+removeChar regex string = ""
+
+addChar :: String -> String -> String
+addChar regex string = ""
+
+changeChar :: String -> String -> String
+changeChar regex string = ""
+
+getPos :: String -> String -> Int -> RTree a
+getPos regex string depth = Node Nil Nil
+getPos regex string 0 = Nil
