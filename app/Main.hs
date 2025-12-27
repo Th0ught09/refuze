7e48706f0dd670f89b7fee8e7a8d97e2ea5b26ae
Kirk Matt <kirkmatt@proton.me>
2025-12-27 18:41:55 +0000

    updated data type to used parametrisation for access

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import Text.Regex.TDFA
 pureGen :: StdGen
 pureGen = mkStdGen 137
 
-data RTree a = Nil | Node String (RTree a) (RTree a)
+data RTree a = Nil | Node {value :: String, left :: RTree a, right :: RTree a} deriving (Show)
 
 main :: IO ()
 main = do
@@ -45,7 +45,7 @@ startTree regex string depth = Node string (getNeg regex string depth pureGen) (
 
 getNeg :: String -> String -> Int -> StdGen -> RTree a
 getNeg regex string depth gen =
-  let rand = getRanIndex string gen 
+  let rand = getRanIndex string gen
       new_gen = snd rand
       newString = badMatch regex string rand
    in Node newString (getNeg regex newString (depth - 1) new_gen) (getPos regex newString (depth - 1) new_gen)
