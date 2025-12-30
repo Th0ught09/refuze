efdcb6147de0f9e187c678235e1282551ee3dd15
Kirk Matt <kirkmatt@proton.me>
2025-12-30 16:43:36 +0000

    updated unit test settings and character added or modified is more random

--- app/Main.hs
+++ app/Main.hs
@@ -76,27 +76,26 @@ posMatch regex string rand
 getNewString :: String -> (Int, StdGen) -> String
 getNewString string rand =
   let choice = fst $ uniformR (1 :: Int, 3) (snd rand)
+      random_char = chr $ fst $ uniformR (32 :: Int, 126) (snd rand)
    in case choice of
         1 -> removeChar string (fst rand)
-        2 -> addChar string (fst rand)
-        _ -> changeChar string (fst rand)
+        2 -> addChar string (fst rand) random_char
+        _ -> changeChar string (fst rand) random_char
 
 removeChar :: String -> Int -> String
 removeChar string index =
   let splitUp = splitAt index string
    in fst splitUp ++ tail (snd splitUp)
 
-addChar :: String -> Int -> String
-addChar string index =
-  let char = chr $ fst $ uniformR (32, 126) pureGen
-      splitUp = splitAt index string
-   in fst splitUp ++ [char] ++ snd splitUp
+addChar :: String -> Int -> Char -> String
+addChar string index random_char =
+  let splitUp = splitAt index string
+   in fst splitUp ++ [random_char] ++ snd splitUp
 
-changeChar :: String -> Int -> String
-changeChar string index =
-  let char = chr $ fst $ uniformR (32, 126) pureGen
-      splitUp = splitAt index string
-   in fst splitUp ++ [char] ++ tail (snd splitUp)
+changeChar :: String -> Int -> Char -> String
+changeChar string index random_char =
+  let splitUp = splitAt index string
+   in fst splitUp ++ [random_char] ++ tail (snd splitUp)
 
 getRanIndex :: String -> StdGen -> (Int, StdGen)
 getRanIndex string gen = uniformR (1 :: Int, length string :: Int) gen
