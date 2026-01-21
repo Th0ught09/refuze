24b55820c22386378ad137824c846b9169f0533a
Kirk Matt <kirkmatt@proton.me>
2026-01-21 12:57:13 +0000

    removed unneeded vec imports

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,11 +3,8 @@ module Main where
 import Refuze.Match
 import Refuze.String
 import System.Random
-import Test.Hspec
 import Test.Tasty
 import Test.Tasty.HUnit
-import Tests.Command
-import Tests.Match
 
 pureGen = mkStdGen 137
 
